b1987162d0986293b3a7fa1605fdbf1b34dc9e69
LENOVO <singhalkritarth@gmail.com>
2020-01-31 23:35:44 +0530

    New Commit

--- app/src/main/java/com/alfazteremere/ui/mobilelogin/MobileLoginActivityModel.java
+++ app/src/main/java/com/alfazteremere/ui/mobilelogin/MobileLoginActivityModel.java
@@ -1,15 +1,19 @@
 package com.alfazteremere.ui.mobilelogin;
 
+import androidx.databinding.BaseObservable;
 import androidx.databinding.Bindable;
+import androidx.databinding.library.baseAdapters.BR;
 
-public class MobileLoginActivityModel {
+public class MobileLoginActivityModel extends BaseObservable {
     String mobile;
 
+    @Bindable
     public String getMobile() {
         return mobile;
     }
 
     public void setMobile(String mobile) {
         this.mobile = mobile;
+        notifyPropertyChanged(BR.mobile);
     }
 }
